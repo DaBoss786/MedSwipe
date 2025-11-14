c2d12ca5718b4cc49862d9c5a015df2a3bfa9762
Abbas Anwar <abbas.a.anwar@gmail.com>
2025-11-13 19:11:20 -0800

    trying to make this splash screen better

--- docs/app.js
+++ docs/app.js
@@ -776,7 +776,7 @@ document.addEventListener('DOMContentLoaded', async function() { // <-- Made thi
       handleUserRouting(event.detail);
       queueDashboardRefresh();
       
-    }, 4000); // Allow progress bar to finish (~3.5s) before fading splash
+    }, 2100); // Allow progress bar to finish (~3.5s) before fading splash
   }
 });
   
